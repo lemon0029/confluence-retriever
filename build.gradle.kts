1a6dc848380d2311904dcb4badafce9ad4904622
imoyb <imoyb@qq.com>
2023-11-10 23:13:14 +0800

    chore: add ktor-client & serializations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.20"
 }
 
 group = "io.nullptr"
@@ -10,7 +11,17 @@ repositories {
 }
 
 dependencies {
+    implementation("org.slf4j:slf4j-api:2.0.9")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+    implementation("io.ktor:ktor-client-apache:2.3.6")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.6")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.6")
+
+    implementation(kotlin("reflect"))
     testImplementation(kotlin("test"))
+    testImplementation("org.slf4j:slf4j-simple:2.0.9")
 }
 
 tasks.test {
